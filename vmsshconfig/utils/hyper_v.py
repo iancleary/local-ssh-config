286d18597b8abd7465d0700bd334d474895819cd
icleary <Ian.Cleary@viasat.com>
2022-06-11 15:50:24 -0700

    Add hyper-v utils

---
+++ vmsshconfig/utils/hyper_v.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+import subprocess
+
+import typer
+
+def run(cmd):
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+    return completed
+
+def get_hyper_v_ip_address(physical_address:str) -> str:
+    # PowerShell command to run
+    interface_command = "arp -a"
+    info = run(interface_command)
+    if info.returncode != 0:
+        typer.echo("Hyper-V: Powershell (arp -a): An error occured: %s", info.stderr)
+    else:
+        typer.echo("Hyper-V: Powershell (arp -a): Interface command executed successfully!")
+    
+        # for line in info.stdout.split("\n"):
+        #     if "00-15-5d-95-fb-09" in line:
+        #         print(line)
+
+        # print(info.stdout)
+        
+        print("-------------------------")
+        
+        # convert b"" to string
+        output = info.stdout.decode("utf-8")
+
+        #print(type(output))
+        #print(output)
+
+        lines = iter(output.splitlines())
+
+        found = False
+        for line in lines:
+            if physical_address in line:
+                ip_line = line
+                found = True
+                break
+        
+        if not found:
+            typer.echo(f"{physical_address} not found in \n\n{str(lines)}\n\n")
+
+        ip_line_cleaned = [x for x in ip_line.split(" ") if x != ""]
+        ip_address = ip_line_cleaned[0]
+
+        return ip_address
